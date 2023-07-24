a970b7fc5ff72578c9258722e004d8ee23c18b0b
ChechoM <78748563+ChechoM@users.noreply.github.com>
2023-07-24 14:40:37 -0500

    primer commit
    primer commit

---
+++ Parqueadero/Data/PqDBContext.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Parqueadero.Data.Models;
+
+namespace Parqueadero.Data
+{
+    public class PqDBContext : DbContext
+    {
+        public PqDBContext (DbContextOptions<PqDBContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Parqueadero.Data.Models.Client> Client { get; set; } = default!;
+
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+
+            modelBuilder.Entity<Client>().ToTable(nameof(Client));
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
