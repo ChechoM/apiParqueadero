74c2e79493f4a023bd00a2356664bb535b3edd3f
Sergio Andres Montoya Gomez <samontoya@cfa.com.co>
2023-08-11 20:05:45 -0500

    instalacion de token

--- Parqueadero/Program.cs
+++ Parqueadero/Program.cs
@@ -1,6 +1,12 @@
-﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.DependencyInjection;
-using Parqueadero.Data;
+﻿using Parqueadero.Data;
+using Parqueadero.Services;
+using HolaMundoAPI.API.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using System.Text;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<PqDBContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("PqDBContext") ?? throw new InvalidOperationException("Connection string 'PqDBContext' not found.")));
@@ -12,19 +18,51 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-var app = builder.Build();
+builder.Services.AddScoped<SeedDb>();
+builder.Services.AddScoped<IUserService, UserService>();
+builder.Services.AddScoped<IAccountService, AccountService>();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+builder.Services.AddSwaggerGen(option =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    option.SwaggerDoc("v1", new OpenApiInfo { Title = "Example API", Version = "v1" });
+    option.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        In = ParameterLocation.Header,
+        Description = "Please enter a valid token",
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        Scheme = JwtBearerDefaults.AuthenticationScheme,
+        BearerFormat = "JWT"
+    });
+    option.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = JwtBearerDefaults.AuthenticationScheme
+                }
+            },
+            new string[]{}
+        }
+    });
+});
 
-app.UseHttpsRedirection();
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme).AddJwtBearer(o =>
+{
+    o.TokenValidationParameters = new TokenValidationParameters
+    {
+        ValidateIssuer = false,
+        ValidateAudience = false,
+        ValidateLifetime = true,
+        ValidateIssuerSigningKey = true,
+        ClockSkew = TimeSpan.Zero,
+        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(builder.Configuration["Jwt:Key"]))
+    };
+});
 
-app.UseAuthorization();
+builder.Services.AddAuthorization();
 
-app.MapControllers();
-
-app.Run();
+var app = builder.Build();
